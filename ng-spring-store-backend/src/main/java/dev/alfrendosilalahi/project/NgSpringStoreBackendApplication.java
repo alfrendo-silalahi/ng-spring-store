8a113216fe3943456f4b92773ace79e26582b8d2
alfrendo-silalahi <alfrendos72@gmail.com>
2024-07-20 16:18:08 +0700

    init NgSpringStore backend project with Spring Boot and MySQL

---
+++ ng-spring-store-backend/src/main/java/dev/alfrendosilalahi/project/NgSpringStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.alfrendosilalahi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSpringStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSpringStoreBackendApplication.class, args);
+	}
+
+}
---
+++ ng-spring-store-backend/src/test/java/dev/alfrendosilalahi/project/NgSpringStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alfrendosilalahi.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgSpringStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
